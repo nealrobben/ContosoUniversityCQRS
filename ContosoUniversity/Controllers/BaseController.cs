04dfe182b8d6ff90e389abbb44749d42b29c5107
Neal Robben <neal.robben@gmail.com>
2020-07-12 23:04:01 +0200

    Made base-controller abstract

--- ContosoUniversity/Controllers/BaseController.cs
+++ ContosoUniversity/Controllers/BaseController.cs
@@ -5,7 +5,7 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace ContosoUniversityCQRS.WebUI.Controllers
 {
-    public class BaseController : Controller
+    public abstract class BaseController : Controller
     {
         protected readonly SchoolContext _context;
 
