127a5a7712107e28daab2e44b22e06386f81602d
Neal Robben <neal.robben@gmail.com>
2020-07-12 15:27:45 +0200

    Added dependency injection for persistence

---
+++ ContosoUniversityCQRS.Persistence/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using ContosoUniversityCQRS.Application.Common.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ContosoUniversityCQRS.Persistence
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<SchoolContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddScoped<ISchoolContext>(provider => provider.GetService<SchoolContext>());
+
+            return services;
+        }
+    }
+}
