20a09ca3d1303ebe92ad74c4fea34af3279e9c0e
Neal Robben <neal.robben@gmail.com>
2020-08-08 14:20:22 +0200

    Used AutoMapper for GetCourseList

--- ContosoUniversityCQRS.Application/Courses/Queries/GetCourseList/CourseSelectionVM.cs
+++ ContosoUniversityCQRS.Application/Courses/Queries/GetCourseList/CourseSelectionVM.cs
@@ -1,11 +1,21 @@
-﻿namespace ContosoUniversityCQRS.Application.Courses.Queries.GetCourseList
+﻿using AutoMapper;
+using ContosoUniversityCQRS.Application.Common.Mappings;
+using ContosoUniversityCQRS.Domain.Entities;
+
+namespace ContosoUniversityCQRS.Application.Courses.Queries.GetCourseList
 {
-    public class CourseSelectionVM
+    public class CourseSelectionVM : IMapFrom<Course>
     {
         public int CourseID { get; set; }
 
         public string Title { get; set; }
 
         public bool Assigned { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<Course, CourseSelectionVM>()
+                .ForMember(d => d.Assigned, opt => opt.Ignore());
+        }
     }
 }
--- ContosoUniversityCQRS.Application/Courses/Queries/GetCourseList/GetCourseListCommandHandler.cs
+++ ContosoUniversityCQRS.Application/Courses/Queries/GetCourseList/GetCourseListCommandHandler.cs
@@ -1,4 +1,6 @@
-﻿using ContosoUniversityCQRS.Application.Common.Interfaces;
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using ContosoUniversityCQRS.Application.Common.Interfaces;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
@@ -11,27 +13,28 @@ namespace ContosoUniversityCQRS.Application.Courses.Queries.GetCourseList
     public class GetCourseListCommandHandler : IRequestHandler<GetCourseListCommand, List<CourseSelectionVM>>
     {
         private readonly ISchoolContext _context;
+        private readonly IMapper _mapper;
 
-        public GetCourseListCommandHandler(ISchoolContext context)
+        public GetCourseListCommandHandler(ISchoolContext context, IMapper mapper)
         {
             _context = context;
+            _mapper = mapper;
         }
 
         public async Task<List<CourseSelectionVM>> Handle(GetCourseListCommand request, CancellationToken cancellationToken)
         {
-            var allCourses = await _context.Courses.AsNoTracking().ToListAsync();
-            var courseIDsForInstructor = await _context.CourseAssignments.Where(x => x.InstructorID == request.InstructorID).Select(x => x.CourseID).ToListAsync();
+            var courses = await _context.Courses
+                .AsNoTracking()
+                .ProjectTo<CourseSelectionVM>(_mapper.ConfigurationProvider)
+                .ToListAsync(cancellationToken);
 
-            var courses = new List<CourseSelectionVM>();
+            var courseIDsForInstructor = await _context.CourseAssignments
+                .Where(x => x.InstructorID == request.InstructorID)
+                .Select(x => x.CourseID).ToListAsync();
 
-            foreach(var course in allCourses)
+            foreach(var course in courses)
             {
-                courses.Add(new CourseSelectionVM
-                {
-                    CourseID = course.CourseID,
-                    Title = course.Title,
-                    Assigned = courseIDsForInstructor.Contains(course.CourseID)
-                });
+                course.Assigned = courseIDsForInstructor.Contains(course.CourseID);
             }
 
             return courses;
